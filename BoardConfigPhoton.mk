aee4d45eec2e6d9007e13ea18375d290d7df689c
Shawn W <wahi.shawn@gmail.com>
2015-11-30 20:52:45 -0500

    Fix typos

--- BoardConfigPhoton.mk
+++ BoardConfigPhoton.mk
@@ -1,7 +1,7 @@
 # Inline kernel
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
-TARGET_KERNEL_SOURCE := kernel/lge/bullhead
-TARGET_KERNEL_CONFIG := bullhead_defconfig
+TARGET_KERNEL_SOURCE := kernel/huawei/angler
+TARGET_KERNEL_CONFIG := angler_defconfig
 
 # Enable vendor image symlink
 BOARD_NEEDS_VENDORIMAGE_SYMLINK := true
