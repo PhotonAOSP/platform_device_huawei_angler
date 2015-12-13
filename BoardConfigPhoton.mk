d1e50bdba10583cfc55a4c948b76bd90c7047117
Shawn W <wahi.shawn@gmail.com>
2015-12-12 20:13:49 -0500

    Disable inline kernel building

--- BoardConfigPhoton.mk
+++ BoardConfigPhoton.mk
@@ -1,9 +1,9 @@
 # Inline kernel
-TARGET_KERNEL_CONFIG := angler_defconfig
-TARGET_KERNEL_SOURCE := kernel/huawei/angler
-KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
-BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+#TARGET_KERNEL_CONFIG := angler_defconfig
+#TARGET_KERNEL_SOURCE := kernel/huawei/angler
+#KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
+#KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
+#BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 
 # Enable vendor image symlink
 BOARD_NEEDS_VENDORIMAGE_SYMLINK := true
